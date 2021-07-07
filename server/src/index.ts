996c9ce5d4819e2e43205434ea537a7e1eb76a79
Марченко Александр Александрович <aamarc@mts.ru>
2021-07-07 22:28:35 +0300

    feat: [back] install express & create simple server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('laa')
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('I Backend service');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
