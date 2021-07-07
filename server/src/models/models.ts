d34a90fe39435a64a19015261e18427768a26379
Марченко Александр Александрович <aamarc@mts.ru>
2021-07-07 21:48:05 +0300

    feat: [back] create interfaces

---
+++ server/src/models/models.ts
@@ -0,0 +1,28 @@
+export interface IForm {
+  columns: IFormColumn[];
+}
+
+export interface IFormColumn {
+  inputs: IFormInput[];
+}
+
+export interface IFormInput {
+  ID: string;
+  inputType: IFormInputType;
+  text: string;
+  required: boolean;
+
+  placeholder?: string;
+}
+
+export type IFormInputType = 'int' | 'string' | 'boolean';
+
+
+export interface IFormData {
+  items: IFormDataItem<string | number | boolean>[];
+}
+
+export interface IFormDataItem<T> {
+  ID: string;
+  value: T;
+}
