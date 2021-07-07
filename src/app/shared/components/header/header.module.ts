e4de8d9b65ed6e474f6dbb17190eeda56a6bca00
Марченко Александр Александрович <aamarc@mts.ru>
2021-07-07 23:00:39 +0300

    feat: [front] created header module

---
+++ src/app/shared/components/header/header.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import { SharedModule } from '../../shared.module';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class HeaderModule { }
