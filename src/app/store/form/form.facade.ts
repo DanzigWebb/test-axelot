d7efdd81f3442e39d5cdccf8f76087e2c6786e26
Марченко Александр Александрович <aamarc@mts.ru>
2021-07-19 01:56:14 +0300

    feat: [front] FormFacade: create new selectors for

--- src/app/store/form/form.facade.ts
+++ src/app/store/form/form.facade.ts
@@ -4,13 +4,14 @@ import { FormState, FormStateModel } from '@src/app/store/form/form.state';
 import { Observable } from 'rxjs';
 import { Form } from '@models/form.model';
 import { FormActions } from '@src/app/store/form/form.actions';
+import { Control } from '@models/control.model';
 
 @Injectable()
 export class FormFacade {
 
   @Select(FormState.state) state$!: Observable<FormStateModel>;
   @Select(FormState.form) form$!: Observable<Form | null>;
-  @Select(FormState.controls) controls$!: Observable<Record<string, string>>;
+  @Select(FormState.controls) controls$!: Observable<Record<string, Control>>;
 
   constructor(
     private store: Store
